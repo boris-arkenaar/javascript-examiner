e00fedda95aa453b99c59edeb7278de1154d8c8b
Oizor <bramnieuw@gmail.com>
2014-12-07 23:00:23 +0100

    Checked my files with check-style.js: Modified accordingly.

--- check-syntax-feedback-formatter.js
+++ check-syntax-feedback-formatter.js
@@ -1,33 +1,30 @@
 var exports = module.exports = {};
 
 exports.parseError = function ParseError (err, src, file) {
-    if (!(this instanceof ParseError)) return new ParseError(err, src, file)
-
-    SyntaxError.call(this);
-
-    this.message = err.message.replace(/^Line \d+: /, '');
-
-    this.line = err.lineNumber;
-    this.column = err.column;
-
-    this.annotated = '\n'
-        //+ (file || '(anonymous file)')
-        //+ ':' + this.line
-        //+ '\n'
-        //+ src.split('\n')[this.line - 1]
-        + Array(this.column).join(' ') + '^'
-        + '\n'
-        + 'ParseError: ' + this.message
-    ;
+  if (!(this instanceof ParseError)) {
+    return new ParseError(err, src, file);
+  }
+  SyntaxError.call(this);
+  this.message = err.message.replace(/^Line \d+: /, '');
+  this.line = err.lineNumber;
+  this.column = err.column;
+  this.annotated = '\n' +
+      //+ (file || '(anonymous file)')
+      //+ ':' + this.line
+      //+ '\n'
+      //+ src.split('\n')[this.line - 1]
+      Array(this.column).join(' ') + '^' +
+      '\n' +
+      'ParseError: ' + this.message;
 }
 
 exports.parseError.prototype = Object.create(SyntaxError.prototype);
-exports.parseError.prototype.constructor = exports.parseError
+exports.parseError.prototype.constructor = exports.parseError;
 
-exports.parseError.prototype.toString = function () {
-    return this.annotated;
+exports.parseError.prototype.toString = function() {
+  return this.annotated;
 };
 
-exports.parseError.prototype.inspect = function () {
-    return this.annotated;
+exports.parseError.prototype.inspect = function() {
+  return this.annotated;
 };
--- check-syntax.js
+++ check-syntax.js
@@ -1,26 +1,22 @@
 var esprima = require('esprima');
 var abSynTree;
-
-
-
-var options = {	tolerant:true, 
-				loc: true, 
-				range: true, 
-				raw: true, 
-				tokens: true, 
-				comment:true};
+var options = {tolerant:true,
+		loc: true,
+		range: true,
+		raw: true,
+		tokens: true,
+		comment:true};
 
 module.exports = function(js, callback, filename) {
-	try {
-		abSynTree = esprima.parse(js, options);
-		callback(null, abSynTree);
-	} catch (e) {
-		//Aard error etc.
-		/*for(var propertyName in e) {
-			console.log(propertyName + ': ' + e[propertyName]);
-		}*/	
-		callback(e);
-		
-		//callback(e);
-	}
+  try {
+    abSynTree = esprima.parse(js, options);
+    callback(null, abSynTree);
+  } catch (e) {
+	//Aard error etc.
+	/*for(var propertyName in e) {
+	console.log(propertyName + ': ' + e[propertyName]);
+	}*/
+    callback(e);
+	//callback(e);
+  }
 }
