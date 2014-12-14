e272c053129233f26642ecca5d22d432c370410b
Boris Arkenaar <boris@arkenaar.com>
2014-12-14 17:02:51 +0100

    Working on trying out our code in the seed project.

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -6,13 +6,35 @@
 
 var app = require('../app');
 
+
+
+'use strict';
+var fs = require('fs');
+var checkFormat = require('./check-format');
+var solutionFile = './model-solution.js';
+
+
+
+
 function index(req, res) {
   res.render('home/index');
 }
 
 function express(req, res) {
-  res.render('home/express', {
-    hello: 'Hello from express world!'
+  fs.readFile(solutionFile, 'utf8', function(err, data) {
+    if (err) {
+      cb(err);
+    }
+    checkFormat(data, function(err, errors) {
+      if (err) {
+        console.log(err)
+      }
+
+      res.render('home/express', {
+        hello: 'Hello from express world!',
+        errors: errors
+      });
+    });
   });
 }
 
