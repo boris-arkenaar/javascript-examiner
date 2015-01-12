ae5fe5f76dbe561085afc340c6145f9391c9a458
Bram Nieuwenhuize <bramnieuw@gmail.com>
2015-01-12 16:03:24 +0100

    minor changes

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.post('/file-upload', function (req, res, next) {
 var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('JavaScript-Examiner listening at http://%s:%s', host, port)
 })
 
 //send form:
