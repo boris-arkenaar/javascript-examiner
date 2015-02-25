8adf7c5c05ad9569094f35ec54c2d9f6e8906ae3
RonaldKluft <kluftr@gmail.com>
2015-02-25 17:26:55 +0100

    Added tests and changed gulp file to call them

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 
-gulp.task('serve', ['lint'], function() {
+gulp.task('serve', ['lint', 'test1', 'test2', 'test3'], function() {
   nodemon({
     script: 'server/server.js',
     ext: 'html json js',
@@ -20,6 +21,21 @@ gulp.task('lint', function() {
       .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'))
       .pipe(jscs());
+});
+
+gulp.task('test1', function() {
+    return gulp.src('server/test/check-syntax-test.js', {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('test2', function() {
+    return gulp.src('server/test/check-format-test.js', {read: false})
+        .pipe(mocha());
+});
+
+gulp.task('test3', function() {
+    return gulp.src('server/test/check-functionality-test.js', {read: false})
+        .pipe(mocha());
 });
 
 gulp.task('default', ['serve']);
