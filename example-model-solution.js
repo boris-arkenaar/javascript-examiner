ea6bb26e68815beb0145667f0236c931289da977
Bram Nieuwenhuize <bramnieuw@gmail.com>
2015-03-05 12:31:54 +0100

    Modified example solution so it passes all tests

--- example-model-solution.js
+++ example-model-solution.js
@@ -1,7 +1,6 @@
-var OK = "";
-var NOK = "";
+var OK = '';
+var NOK = '';
 var EPS = 1e-2;
-
 
 /*
 function calcBMI(lengte:integer,gewicht:number)->numberGoal: berekent BMI-index op basis van gewicht(kg)/lenget(m)^2
@@ -11,18 +10,20 @@ Parameters:
            (minimum 75 cm, maximum 225 cm)
   gewicht: lichaamsgewicht in kg
            (minimum 30 kg, maximum 250 kg)
-		   
+       
 Returns: de BMI-indexThrows exeption als een of meer parameters een waarde
 hebben buiten aangegeven interval
-		   
+       
 */
 
 function calcBMI(lengte, gewicht) {
   if (lengte < 75 ||
       lengte > 225 ||
       gewicht < 30 ||
-      gewicht > 250) {(((((((()))
-    throw new Error("Lengte of gewicht onjuist: lengte moet tussen 75 en 225 liggen; gewicht tussen 30 en 250");
+      gewicht > 250) {
+    throw new Error('Lengte of gewicht onjuist: ' +
+                    'lengte moet tussen 75 en 225 liggen' +
+                    '; gewicht tussen 30 en 250');
   }
   return Math.round(gewicht / ((lengte / 100) * (lengte / 100)));
 }
