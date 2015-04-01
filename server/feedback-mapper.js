d9c94a72aedcbeca208b772bfcbfe8de75ba6f1e
RonaldKluft <kluftr@gmail.com>
2015-04-01 12:26:34 +0200

    Added callback function

--- server/feedback-mapper.js
+++ server/feedback-mapper.js
@@ -1,12 +1,18 @@
 var fs = require('fs');
+var description;
+var module;
 
 module.exports = function(check, feedback) {
   var src;
   var trg;
   var pos;
-  var module = check;
-  searchError(module, feedback);
-  return feedback;
+  var callback = function(value) {
+    console.log('callback ' + value);
+    return value;
+  };
+  module = check;
+  searchError(module, feedback, callback);
+  return feedback.description;
 };
 
 function generateFileName(name) {
@@ -16,7 +22,7 @@ function generateFileName(name) {
   return localName;
 }
 
-function createFile(filename) {
+function createFile(filename, callback) {
   fs.writeFile(filename, '', function(err) {
     if (err) {
       return console.log(err);
@@ -31,37 +37,46 @@ function checkFile(name) {
   });
 }
 
-function searchError(module, feedback) {
+function searchError(module, feedback, callback) {
 // search for error, if not found create error with description is equal to error
 // read file and put the content in an array
 // then check the feedback with items from the array
 // if found, replace feedback with entrance
 // otherwise, add feedback to file
   var fn = generateFileName(module);
-  var contents = fs.readFileSync(fn).toString();
-  var found = false;
-  if (contents.toString().trim() !== '') {
-    var lines = contents.toString().split('\n');
-    lines.map(function(e, i, a) {
-      if (e.trim() === '') {
-        lines.splice(i, 1);
-      }
-    });
-    lines.map(function(x) {
-      src = x.split(';');
-      if (feedback.description === src[0]) {
-        found = true;
-        feedback.description = src[1];
-      }
-    });
-  }
-  if (found === false) {
-    // append description to file
-    var descr = feedback.description;
-    fs.appendFile(fn, descr + ';' + descr + '\n', function(err) {
+  description = feedback.description;
+  fs.open(fn, 'a+', function() {
+    fs.readFile(fn, function(err, buf) {
       if (err) {
         return console.log(err);
       }
+      var found = false;
+      if (buf.toString().trim() !== '') {
+        var lines = buf.toString().split('\n');
+        var found = false;
+        lines.map(function(e, i, a) {
+          if (e.trim() === '') {
+            lines.splice(i, 1);
+          }
+        });
+        lines.map(function(x) {
+          src = x.split(';');
+          if (description === src[0]) {
+            found = true;
+            description = src[1];
+          }
+        });
+      }
+      if (found === false) {
+        // append description to file
+        var descr = feedback.description;
+        fs.appendFile(fn, descr + ';' + descr + '\n', function(err) {
+          if (err) {
+            return console.log(err);
+          }
+        });
+      }
+      callback(description);
     });
-  }
+  });
 }
