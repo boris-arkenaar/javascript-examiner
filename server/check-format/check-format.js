dfd5df539a43ce77fc222d4713000ad6cb1751c7
Oizor <bramnieuw@gmail.com>
2015-04-05 10:48:30 +0200

    gh-45: Added unittest, refactored check-format, noticed bug related to #14, asked @RonaldKluft to fix this.

--- server/check-format/check-format.js
+++ server/check-format/check-format.js
@@ -4,14 +4,14 @@ var Objects = require('../objects');
 var mapper = require('../feedback-mapper');
 
 module.exports = function(submitted, cb) {
+  //configure checker
   var checker = new Checker();
   checker.registerDefaultRules();
   checker.configure(loadConfigFile.load(__dirname + '/jscs-config.json'));
-
   var feedbackList = [];
-  //var result = [];
   try {
     var errors = checker.checkString(submitted.code);
+    //loop over errors
     errors.getErrorList().forEach(function(err) {
       var feedback = new Objects.Feedback();
       feedback.addressee = 'student';
@@ -19,112 +19,14 @@ module.exports = function(submitted, cb) {
       feedback.description = err.message;
       feedback.line = err.line;
       feedback.column = err.column;
-      mapper('check-syntax', feedback, function(value) {
-        feedback.description = value;
-        feedbackList.push(feedback);
-      });
+      //This should be fixed, see comment #14
+      //mapper('check-syntax', feedback, function(value) {
+        // feedback.description = value;
+      feedbackList.push(feedback);
+      //});
     });
   } catch (err) {
     cb(err);
   }
-  console.log('success: Check-Format');
   cb(null, feedbackList);
-
 };
-
-function ignoreComments(plainCode) {
-  // console.log(typeof plainCode);
-  // console.log(plainCode.indexOf('/*'));
-  // plainCode = plainCode.replace('/*', '/* jscs: disable')
-  //     .replace('*/', '*/ \n //jscs: enable');
-  // console.log(plainCode);
-  return plainCode;
-}
-
-/*
-  This function is loosely based on the one found here:
-  http://www.weanswer.it/blog/optimize-css-javascript-remove-comments-php/
-*/
-function removeComments(str) {
-  str = ('__' + str + '__').split('');
-  var mode = {
-    singleQuote: false,
-    doubleQuote: false,
-    regex: false,
-    blockComment: true,
-    lineComment: false,
-    condComp: false
-  };
-  for (var i = 0, l = str.length; i < l; i++) {
-
-    if (mode.regex) {
-      if (str[i] === '/' && str[i - 1] !== '\\') {
-        mode.regex = false;
-      }
-      continue;
-    }
-
-    if (mode.singleQuote) {
-      if (str[i] === '\'' && str[i - 1] !== '\\') {
-        mode.singleQuote = false;
-      }
-      continue;
-    }
-
-    if (mode.doubleQuote) {
-      if (str[i] === '"' && str[i - 1] !== '\\') {
-        mode.doubleQuote = false;
-      }
-      continue;
-    }
-
-    if (mode.blockComment) {
-      if (str[i] === '*' && str[i + 1] === '/') {
-        str[i + 1] = '';
-        mode.blockComment = false;
-      }
-      str[i] = '';
-      continue;
-    }
-
-    if (mode.lineComment) {
-      if (str[i + 1] === '\n' || str[i + 1] === '\r') {
-        mode.lineComment = false;
-      }
-      str[i] = '';
-      continue;
-    }
-
-    if (mode.condComp) {
-      if (str[i - 2] === '@' && str[i - 1] === '*' && str[i] === '/') {
-        mode.condComp = false;
-      }
-      continue;
-    }
-
-    mode.doubleQuote = str[i] === '"';
-    mode.singleQuote = str[i] === '\'';
-
-    if (str[i] === '/') {
-
-      if (str[i + 1] === '*' && str[i + 2] === '@') {
-        mode.condComp = true;
-        continue;
-      }
-      if (str[i + 1] === '*') {
-        str[i] = '';
-        mode.blockComment = true;
-        continue;
-      }
-      if (str[i + 1] === '/') {
-        str[i] = '';
-        mode.lineComment = true;
-        continue;
-      }
-      mode.regex = true;
-
-    }
-
-  }
-  return str.join('').slice(2, -2);
-}
--- server/check-format/check-format.test.js
+++ server/check-format/check-format.test.js
@@ -1,10 +1,38 @@
 var assert = require('assert');
-var checkFormat = require('../check-format/check-format');
+var checkFormat = require('./check-format');
 
-var testregel = 'function calcBMI() {};';
-
-describe('Functie check-format', function() {
-  it('Test check-format', function() {
-    //assert.equal(checkFormat(testregel, function() {}), 'undefined');
+describe('check-format.js', function() {
+  it('should Export a single function', function() {
+    assert.equal('function', typeof checkFormat);
+  });
+  it('should report no feedback when input is ok', function(done) {
+    var input = {
+      code: 'function calcBMI() {};\n'
+    };
+    checkFormat(input, function(err, feedback) {
+      assert.equal(0, feedback.length);
+    });
+    done();
+  });
+  it('should report feedback when input has a flaw', function(done) {
+    var input = {
+      //code has trailing whitespace at end of line
+      code: 'function calcBMI() {};  \n'
+    };
+    checkFormat(input, function(err, feedback) {
+      assert.equal(1, feedback.length);
+      done();
+    });
+  });
+  it('should report feedback when input has multiple flaws', function(done) {
+    var input = {
+      //code has trailing whitespace at end of line
+      //code has no empty line at end of file
+      code: 'function calcBMI() {};  '
+    };
+    checkFormat(input, function(err, feedback) {
+      assert.equal(2, feedback.length);
+      done();
+    });
   });
 });
