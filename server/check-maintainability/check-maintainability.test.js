5dc37e926a44a30a0facb96c1c1106aa35f93b08
Oizor <bramnieuw@gmail.com>
2015-04-09 13:01:16 +0200

    gh-45 Added unittest

--- server/check-maintainability/check-maintainability.test.js
+++ server/check-maintainability/check-maintainability.test.js
@@ -1,10 +1,21 @@
-var assert = require('assert');
-var checkMntnblty = require('../check-maintainability/check-maintainability');
+var assert = require('chai').assert;
+var checkMaintainability = require('./check-maintainability');
 
-var testregel = 'function calcBMI() {};';
-
-describe('Functie check-maintainability', function() {
-  it('Test check-maintainability', function() {
-    //assert.equal(checkMntnblty(testregel, function() {}), 'undefined');
+describe('check-maintainability.js', function() {
+  it('should Export a single function', function() {
+    assert.equal('function', typeof checkMaintainability);
+  });
+  it('should return only artefacts', function(done) {
+    var input = {
+      //code has trailing whitespace at end of line
+      //code has no empty line at end of file
+      code: 'function calcBMI() {};  '
+    };
+    checkMaintainability(input, function(err, feedback, artefacts) {
+      assert.isNull(feedback);
+      assert.isNull(err);
+      assert.isObject(artefacts);
+      done();
+    });
   });
 });
